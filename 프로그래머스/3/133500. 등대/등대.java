31f4f5513c86fbb9b65a293b75cdea5edca62350
snsn8105 <124420738+snsn8105@users.noreply.github.com>
2026-02-07 16:49:04 +0900

    [level 3] Title: 등대, Time: 70.37 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -6,28 +6,21 @@ class Solution {
 
     // 반환값: 해당 노드가 불을 켰는지 여부 (0: 안 켬, 1: 켬)
     int dfs(int cur, int parent) {
-        // 자식 노드가 하나도 없는 리프 노드라면 불을 켜지 않고 0 반환
-        if (adj[cur].size() == 1 && adj[cur].get(0) == parent) {
-            return 0;
-        }
-
-        int childState = 0;
-        for (int next : adj[cur]) {
-            if (next == parent) continue;
-            // 자식들의 상태를 확인
-            // 자식 중 하나라도 0(불 안 켬)을 반환하면, 현재 노드(부모)는 불을 켜야 함
-            if (dfs(next, cur) == 0) {
+        int childState = 0; // 1이면 내가 불을 켜야함
+        
+        for (int a : adj[cur]) {
+            if (a == parent)
+                continue;
+            if (dfs(a, cur) == 0) {
                 childState = 1;
             }
         }
-
-        // 자식 중 불 안 켠 애가 있어서 내가 켜야 하는 경우
+        
         if (childState == 1) {
             answer++;
-            return 1; // 내가 불을 켰음을 부모에게 알림
-        }
-
-        return 0; // 나는 불을 켜지 않았음
+            return childState;
+        } else
+            return childState;
     }
 
     public int solution(int n, int[][] lighthouse) {
@@ -40,7 +33,7 @@ class Solution {
         }
 
         // 1번 노드를 루트로 시작 (트리이므로 어디서 시작해도 무관)
-        dfs(1, 0);
+        dfs(2, 0);
 
         return answer;
     }
