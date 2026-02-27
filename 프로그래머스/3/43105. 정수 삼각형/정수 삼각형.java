24664525a9fee1e44da527e7a3920af365c0a349
snsn8105 <124420738+snsn8105@users.noreply.github.com>
2026-02-27 11:20:03 +0900

    [level 3] Title: 정수 삼각형, Time: 9.24 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] triangle) {
+        
+        for (int i = triangle.length - 1 ; i >= 0 ; i--) {
+            for (int j = 0 ; j < triangle[i].length - 1 ; j++) {
+                triangle[i-1][j] += Math.max(triangle[i][j], triangle[i][j+1]);
+            }
+        }
+        
+        int answer = triangle[0][0];
+        return answer;
+    }
+}
