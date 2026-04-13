6e72a2170cecaff59d07ff3783abe599f2793246
snsn8105 <124420738+snsn8105@users.noreply.github.com>
2026-04-13 17:52:01 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 3.60 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/README.md
+++ 프로그래머스/3/150365. 미로 탈출 명령어/README.md
@@ -1,10 +1,10 @@
 # [level 3] 미로 탈출 명령어 - 150365 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365?gad_source=1&gad_campaignid=22499034228&gbraid=0AAAAAC_c4nDfOgGgw-9vENK7mzRpU5DXf&gclid=CjwKCAiAkbbMBhB2EiwANbxtbe-xU-NAu3ziSRigH_5ocQVK1Jm0MppEMhtq4Jg0h30GjJRgW3LDehoCgCwQAvD_BwE) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365) 
 
 ### 성능 요약
 
-메모리: 86.9 MB, 시간: 3.52 ms
+메모리: 82.7 MB, 시간: 3.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 13일 10:41:35
+2026년 04월 13일 17:51:57
 
 ### 문제 설명
 
--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,70 +1,54 @@
 import java.util.*;
 class Solution {
-    int[][] map;
+    // d, l, r, u 순으로 반복
+    // 순서대로 d, l, r, u
+    int[] dx = {1, 0, 0, -1}; 
+    int[] dy = {0, -1, 1, 0};
+    char[] dChar = {'d', 'l', 'r', 'u'};
     String answer = "impossible";
-    void dfs(StringBuilder sb, int x, int y, int r, int c, int k) {
-        // base : count == k 이면 종료
-        if (k == sb.length()) {
-            if (x == r && y == c) {
-                if (answer.equals("impossible"))
-                    answer = sb.toString();
-                return;
-            }
-            else
-                return;
-        }
-        // 가지치기 -> k - sb.length() 보다 x-r + y-c가 더 크면 종료
-        int remain = k - sb.length();
-        int distance = Math.abs(x-r) + Math.abs(y-c);
-        
-        if (remain < distance || (remain - distance) % 2 != 0)
+
+    public void dfs(int n, int m, int x, int y, int r, int c, int k, int dist, StringBuilder sb) {
+        // StringBuilder에 사전 순으로 추가하면서 재귀 진행
+        // r,c에 도착하면 answer에 저장
+        // 맨해튼 거리가 k - dist보다 크면 가지치기
+        if (!answer.equals("impossible"))
             return;
         
-        // 사전 순으로 d->l->r->u 순으로 배열 생성
-        int[] dx = {1, 0, 0, -1};
-        int[] dy = {0, -1, 1, 0};
+        int remainDist = Math.abs(x - r) + Math.abs(y - c);
+        
+        if (remainDist > k - dist || (k - dist - remainDist) % 2 != 0 )
+            return;
+        
+        if (dist == k && x == r && y == c) {
+            answer = sb.toString();
+            return;
+        }
         
         for (int i = 0 ; i < 4 ; i++) {
-            if (!inMap(x+dx[i], y+dy[i])) {
-                continue;
-            }
-            if (!answer.equals("impossible"))
-                return;
+            int nx = x + dx[i];
+            int ny = y + dy[i];
             
-            if (i == 0) {
-                sb.append('d');
-                dfs(sb, x+dx[i], y+dy[i], r, c, k);
-                sb.deleteCharAt(sb.length()-1);
-            } else if (i == 1) {
-                sb.append('l');
-                dfs(sb, x+dx[i], y+dy[i], r, c, k);
-                sb.deleteCharAt(sb.length()-1);
-            } else if (i == 2) {
-                sb.append('r');
-                dfs(sb, x+dx[i], y+dy[i], r, c, k);
-                sb.deleteCharAt(sb.length()-1);
-            } else if (i == 3) {
-                sb.append('u');
-                dfs(sb, x+dx[i], y+dy[i], r, c, k);
-                sb.deleteCharAt(sb.length()-1);
-            }
+            if (!isInMap(n, m, nx, ny))
+                continue;
+            
+            sb.append(dChar[i]);
+            dfs(n, m, nx, ny, r, c, k, dist+1, sb);
+            sb.deleteCharAt(sb.length()-1);
         }
-    } 
-    boolean inMap(int x, int y) {
-        int n = map.length;
-        int m = map[0].length;
-        
-        if (x >= 1 && x < n && y >= 1 && y < m)
+    }
+    public boolean isInMap(int n, int m, int x, int y) {
+        if (x >= 0 && x < n && y >= 0 && y < m)
             return true;
         else
             return false;
     }
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        map = new int[n+1][m+1]; //[x][y]가 출발 위치, [r][c]가 도착지
+        // n x m 미로에서 k 만큼 이동해서 도착지 r,c로 가야함
+        // abcdefghijklmnop
+        // d, l, r, u 순
         StringBuilder sb = new StringBuilder();
         
-        dfs(sb, x, y, r, c, k);
-        
+        dfs(n, m, x-1, y-1, r-1, c-1, k, 0, sb);
         return answer;
     }
 }
